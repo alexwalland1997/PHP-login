5a8ceacf73656d7fb45727703cddaf50e56b30f6
alexwalland1997 <146854684+alexwalland1997@users.noreply.github.com>
2024-07-13 11:23:11 +0100

    added styling and nav bar

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Log in system</title>
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-    
+
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="signin.php">Log in</a></li>
+    </ul>
+
+    <h1>Hello World!</h1>
 </body>
 </html>
